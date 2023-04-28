00d8e763c266ab1cb0650e2df8753fa32b3b61e5
hyla167 <107759618+hyla167@users.noreply.github.com>
2023-04-28 18:37:38 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -14,7 +14,7 @@ async function connect() {
     } */
     // mongodb+srv://languyengiahy:I0ELxZEo1ac2zV1N@cluster0.bsu29kk.mongodb.net/uwc_v2
     try{
-        await mongoose.connect('mongodb+srv://languyengiahy:I0ELxZEo1ac2zV1N@cluster0.bsu29kk.mongodb.net/test', {
+        await mongoose.connect('mongodb+srv://tatcatrithuc:vankha123@cluster0.pbwoixc.mongodb.net/uwc2', {
             useNewUrlParser:true,
             useUnifiedTopology: true
         });
